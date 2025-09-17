6a6d9dcb95695049e63993021020dafe572f81f3
Nick Jackson <nick@nejack.com>
2025-09-17 15:50:40 -0400

    Initial Commit

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Nick Jackson on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
